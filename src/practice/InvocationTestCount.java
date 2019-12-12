90b7fe85342cbac0d45c03a86d119c97deeddc7a
jamshed3005 <58817734+jamshed3005@users.noreply.github.com>
2019-12-12 23:37:10 +0530

    Update InvocationTestCount.java

--- src/practice/InvocationTestCount.java
+++ src/practice/InvocationTestCount.java
@@ -7,9 +7,9 @@ public class InvocationTestCount {
 	@Test(invocationCount =10, timeOut=2,expectedExceptions=ArithmeticException.class)
 	public void test1()
 	{
-		int c=10;
+		int s=10;
 		int d=20;
-		int f= c+d;
+		int f= s+d;
 		int i=1/0;
 		
 	}
